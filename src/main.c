d55a8954e9594b0c0f5edf1a9803578ece33f9d9
Simon Jones <github@simonjjones.com>
2023-01-16 09:18:06 -0500

    changing intervals between bitbashing and adding adc readin

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
+#include "hardware/adc.h"
 #include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
 #include "ws2812.pio.h"
 
 static inline void put_pixel(uint32_t pixel_grb) {
@@ -16,7 +17,7 @@ static inline void put_pixel(uint32_t pixel_grb) {
 
 // T0H | 0 code, high voltage time | 0.4us  | ±150ns
 // T1H | 1 code, high voltage time | 0.8us  | ±150ns
-// T0L | 0 code, low voltage time | 0.85us | ±150ns
+// T0L | 0 code, low voltage time  | 0.85us | ±150ns
 // T1L | 1 code, low voltage time  | 0.45us | ±150ns
 // RES | low voltage time          | Above 50µs
 
@@ -35,6 +36,9 @@ struct Rgb {
   uint32_t b;
 };
 
+static struct Hsv hsv1 = {0, 0, 0};
+static struct Hsv hsv2 = {0, 0, 0};
+
 static inline uint32_t urgb_u32(struct Rgb rgb) {
   return
     ((uint32_t)(rgb.r) << 8) |
@@ -50,38 +54,32 @@ static inline struct Rgb hsv_to_rgb(struct Hsv hsv) {
   float x = c * (1 - abs(fmodf((h / 60), 2) - 1));
   float m = v - c;
   float rgb_[3];
-  if (false) { }
-  else if (0 <= h && h < 60) {
+  if (false) {
+  } else if (0 <= h && h < 60) {
     rgb_[0] = c;
     rgb_[1] = x;
     rgb_[2] = 0;
-  }
-  else if (0 <= h && h < 60) {
+  } else if (0 <= h && h < 60) {
     rgb_[0] = c;
     rgb_[1] = x;
     rgb_[2] = 0;
-  }
-  else if (60 <= h && h < 120) {
+  } else if (60 <= h && h < 120) {
     rgb_[0] = x;
     rgb_[1] = c;
     rgb_[2] = 0;
-  }
-  else if (120 <= h && h < 180) {
+  } else if (120 <= h && h < 180) {
     rgb_[0] = 0;
     rgb_[1] = c;
     rgb_[2] = x;
-  }
-  else if (180 <= h && h < 240) {
+  } else if (180 <= h && h < 240) {
     rgb_[0] = 0;
     rgb_[1] = x;
     rgb_[2] = c;
-  }
-  else if (240 <= h && h < 300) {
+  } else if (240 <= h && h < 300) {
     rgb_[0] = x;
     rgb_[1] = 0;
     rgb_[2] = c;
-  }
-  else if (300 <= h && h < 360) {
+  } else if (300 <= h && h < 360) {
     rgb_[0] = c;
     rgb_[1] = 0;
     rgb_[2] = x;
@@ -94,21 +92,41 @@ static inline struct Rgb hsv_to_rgb(struct Hsv hsv) {
   return result;
 }
 
+static inline struct Hsv get_hsv_adc() {
+  adc_select_input(0); // gp26
+  uint16_t h = ((uint16_t)adc_read()) / (1 << 12) * 360;
+  adc_select_input(1); // gp27
+  uint16_t s = ((uint16_t)adc_read()) / (1 << 12) * 100;
+  adc_select_input(2); // gp28
+  uint16_t v = ((uint16_t)adc_read()) / (1 << 12) * 100;
+  struct Hsv result = {
+    .h = h,
+    .s = s,
+    .v = v,
+  };
+  return result;
+}
 
 static inline void blink() {
-  gpio_init(led_pin);
+  gpio_init(led_pin); // led
   gpio_set_dir(led_pin, 1);
 
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 5; i++) {
     gpio_put(led_pin, 1);
-    sleep_ms(250);
+    sleep_ms(100);
     gpio_put(led_pin, 0);
-    sleep_ms(250);
+    sleep_ms(100);
   }
 }
 
 int main() {
   stdio_init_all();
+  adc_init();
+  gpio_init(26); // adc0
+  gpio_init(27); // adc1
+  gpio_init(28); // adc2
+  gpio_init(1); // toggle hsv1
+  gpio_set_dir(1, 0);
 
   uint pin_number = 0;
 
@@ -117,49 +135,77 @@ int main() {
 
   ws2812_program_init(pio, 0, offset, pin_number, 800000);
 
-  // FIXME: pull from pots
-  struct Hsv hsv1 = {
-    .h = 0.0,
-    .s = 1.0,
-    .v = 1.0
-  };
-  struct Hsv hsv2 = {
-    .h = 0.0,
-    .s = 1.0,
-    .v = 1.0
-  };
-
-  float delta = hsv2.h - hsv1.h;
-  if (0) {}
-  else if (360 > delta && delta > 180) {
-    delta = 360 - delta;
-  } else if (0 <= delta && delta <= 180) {
-    // do nothing
-  } else if (0 > delta && delta > -180) {
-    // do nothing
-  } else if (-180 >= delta && delta >= -360) {
-    delta = 360 + delta;
-  }
-  float h_step = delta / num_pixels;
-  float s_step = (hsv2.s - hsv1.s) / num_pixels;
-  float v_step = (hsv2.v - hsv1.v) / num_pixels;
-  struct Hsv hsv_current = hsv1;
-  struct Rgb rgbs[num_pixels];
-  for (int i = 0; i < num_pixels; i++) {
-    rgbs[i] = hsv_to_rgb(hsv_current);
-    hsv_current.h += h_step;
-    if (hsv_current.h < 0)
-      hsv_current.h += 360;
-    if (hsv_current.h > 360)
-      hsv_current.h -= 360;
-    hsv_current.s += s_step;
-    hsv_current.v += v_step;
-  }
-
   while (1) {
+    blink();
+    // read hsv input from adc pins & toggle current through each pot rapidly
+    /* struct Hsv temphsv = get_hsv_adc();
+    // check to see if current input differs from other side
+    // if it differs, replace the corresponding hsv with the correct one
+    if (gpio_get(1)) {
+      // if switch is toggled to the left (on)
+      if (1
+        && temphsv.h == hsv2.h
+        && temphsv.s == hsv2.s
+        && temphsv.v == hsv2.v) {
+        // do nothing
+      } else {
+        hsv1 = temphsv;
+      }
+    } else {
+      // if switch is toggled to the right (off)
+      if (1
+        && temphsv.h == hsv1.h
+        && temphsv.s == hsv1.s
+        && temphsv.v == hsv1.v) {
+        // do nothing
+      } else {
+        hsv2 = temphsv;
+      }
+    } */
+
+    hsv1.h = 246;
+    hsv1.s = 69;
+    hsv1.v = 49;
+
+    hsv2.h = 285;
+    hsv2.s = 83;
+    hsv2.v = 96;
+
+    // create an array of rgb pixels
+    float delta = hsv2.h - hsv1.h;
+    if (0) {}
+    else if (360 > delta && delta > 180) {
+      delta = 360 - delta;
+    } else if (0 <= delta && delta <= 180) {
+      // do nothing
+    } else if (0 > delta && delta > -180) {
+      // do nothing
+    } else if (-180 >= delta && delta >= -360) {
+      delta = 360 + delta;
+    }
+    float h_step = delta / num_pixels;
+    float s_step = (hsv2.s - hsv1.s) / num_pixels;
+    float v_step = (hsv2.v - hsv1.v) / num_pixels;
+    struct Hsv hsv_current = hsv1;
+    struct Rgb rgbs[num_pixels];
+    for (int i = 0; i < num_pixels; i++) {
+      // FIXME
+      // rgbs[i] = hsv_to_rgb(hsv_current);
+      rgbs[i].r = 194;
+      rgbs[i].g = 41;
+      rgbs[i].b = 244;
+      hsv_current.h += h_step;
+      if (hsv_current.h < 0)
+        hsv_current.h += 360;
+      if (hsv_current.h > 360)
+        hsv_current.h -= 360;
+      hsv_current.s += s_step;
+      hsv_current.v += v_step;
+    }
+    // push the pixels sequentially out of the stack
     for (int i = 0; i < num_pixels; i++) {
       put_pixel(urgb_u32(rgbs[i]));
     }
-    sleep_ms(500);
+    sleep_ms(1000);
   }
 }
